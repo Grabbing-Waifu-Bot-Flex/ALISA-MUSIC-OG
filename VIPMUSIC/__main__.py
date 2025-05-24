e9f0b16437aa26579738920ba69d54eba6a7db11
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2025-05-24 20:43:58 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -1,15 +1,8 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
 import asyncio
 import importlib
 
 from pyrogram import idle
+import pyrogram
 
 import config
 from config import BANNED_USERS
